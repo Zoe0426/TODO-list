e7bbf17f8cf3a20a756f153dc2de59b787646dc5
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-11-21 18:36:33 +0800

    test

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -14,7 +13,10 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Link href={"/login"}><h1>點我去登入</h1></Link>
+      <Link href={"/login"}>
+        <h1>點我去登入</h1>
+      </Link>
+      <div></div>
     </>
   );
 }
